e6dd6359357db2331ee2e91e5fa9fdd0cdb60917
aallison06 <aaa.7701@outlook.com>
2023-04-25 09:44:00 -0400

    Import 1

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const host = 'localhost';
+const port = 3333;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Hello World.");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is operational on http://${host}:${port}`);
+})
