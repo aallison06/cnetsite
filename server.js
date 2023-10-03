448609090735e23f191f0d22cff8dd3dcda87277
aallison06 <131303295+aallison06@users.noreply.github.com>
2023-10-03 13:13:47 -0400

    FIX

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http'); // Beginning of URL
 const host = 'localhost'; // URL
-const PORT = 3223; // Port
+const PORT = 8080; // Port
 const express = require('express'); // Express dep
 var fs = require('fs'); //File Share
 const { error } = require('console');
@@ -97,55 +97,3 @@ if (typeof error === "undefined") {
 }
 
 sleep(2000);
-
-/*
-// Browser Compatibility Script 2
-if (typeof document.querySelector !== "undefined") {
-    console.log('querySelector is supported.');
-    // querySelector is supported
-} else {
-    console.log('querySelector is not supported.');
-    // querySelector is not supported
-} */
-/*
-// Mozilla Firefox Compatibility Script
-if (typeof document.body.style.MozUserSelect !== "undefined") {
-    // Firefox is detected
-    document.body.style.MozUserSelect = "none";
-} else {
-    // Other browsers
-    document.body.style.userSelect = "none";
-} */
-/*
-var pages = new Array();
-
-function preloadPages(){
-    for (i=0; i < preloadPages.arguments.length; i++){
-        pages [i] = new Page();
-        pages[i].src = preloadPages.arguments[i];
-    }
-}
-preloadPages('page.html', 'credit.html', 'contact.html', 'about.html');
-
-// image preload attempt
-var images = new Array();
-
-function preloadImages(){
-    for (i=0; i < preloadImages.arguments.length; i++){
-        images [i] = new Image();
-        images[i].src = preloadImages.arguments[i];
-    }
-}
-preloadImages('logocnet.png');
-*/
-/*
-if(error){
-    throw error;
-} else {
-    console.log('No errors present.')
-}
-
-// Error support script
-if(typeof error === "undefined") {
-    console.log("A critical error has occurred. Please contact site administrator or developer.");
-} */
